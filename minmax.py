92b2c43ed802595ca9f52db8acad6c698ca8f50d
Bhavik Jain <bhavikjain403@gmail.com>
2022-06-09 18:27:11 +0530

    Added minmax

---
+++ minmax.py
@@ -0,0 +1,24 @@
+def minmax(arr):
+    while len(arr)>1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+        minmax(left)
+        minmax(right)
+        minl = min(left)
+        maxl = max(left)
+        maxr = max(right)
+        minr = min(right)
+        if minl>=minr:
+            mini = minr
+        else:
+            mini = minl
+        if maxl>=maxr:
+            maxi = maxl
+        else:
+            maxi = maxr
+        return mini, maxi
+
+l = list(map(int, input("Enter the list of numbers : ").split()))
+mini,maxi = minmax(l)
+print(mini, maxi)
